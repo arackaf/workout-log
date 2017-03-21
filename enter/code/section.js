ca06c95cfbb2665b69797424dbe0e8b05b4a984a
Adam Rackis <adamrackis@gmail.com>
2017-03-20 23:31:24 -0500

    react-select rendering

--- enter/code/section.js
+++ enter/code/section.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
 import {FadeList} from 'util/fade';
+import Select, {Creatable} from 'react-select';
+
 
 @observer
 class Line extends Component {
     componentDidMount() {
-        this.input.focus();   
+        this.input.focus();
     }    
     render() {
         let {store} = this.props;
@@ -17,12 +19,28 @@ class Line extends Component {
     }
 }
 
+var options = [
+	{ value: 1, label: 'One' },
+	{ value: 2, label: 'Two' },
+	{ value: 3, label: 'Three' },
+	{ value: 4, label: 'Four' },
+	{ value: 5, label: 'Five' },
+];
+
 @observer
 export default class Section extends Component {
+    state = {tags: []};
+    handleSelectChange = value => {
+        console.log('You have selected: ', value);
+        this.setState({ tags: value });
+    }
     render() {
         let {store} = this.props;
         return (
             <div className='panel panel-default' style={{float: 'left', padding: '15px', margin: '5px', minWidth: '350px'}}>
+                <div className="form-group">
+                    <Creatable onChange={this.handleSelectChange} value={this.state.tags} multi={true} options={options} />
+                </div>
                 <div className="form-group">
                     <textarea className="form-control" rows="3" placeholder="Notes" />
                 </div>
