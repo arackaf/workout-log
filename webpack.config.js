fcb45f3418160c2353ff13b092202bf3edcfb602
Adam Rackis <adamrackis@gmail.com>
2017-04-09 22:59:20 -0500

    update build stuff

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015-webpack', 'stage-1', 'stage-2'],
+                    presets: ['react', 'es2015', 'stage-2'],
                     plugins: ['transform-decorators-legacy']
                 }
             }
